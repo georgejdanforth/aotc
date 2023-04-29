ff6537a65e5f203d10eb845d7d771898bb3fa447
George Danforth <georgejdanforth@gmail.com>
2023-04-29 17:25:42 -0400

    Add context injection with database conn

--- aotc/__main__.py
+++ aotc/__main__.py
@@ -1,8 +1,11 @@
 import logging
 import logging.config
 import tomllib
+import typing as t
+from functools import wraps
 
 import click
+import psycopg
 
 from ._logging import LOGGING
 logging.config.dictConfig(LOGGING)
@@ -13,9 +16,28 @@ from .config import Config
 logger = logging.getLogger(__name__)
 
 
-def _load_config() -> Config:
+class Context(t.NamedTuple):
+    config: Config
+    connection: psycopg.Connection
+
+
+def load_config() -> Config:
     with open("config.toml", "rb") as f:
         return Config(**tomllib.load(f))
+
+
+def inject_context(cmd: t.Callable) -> t.Callable:
+    logger.info("Loading configuration")
+    config = load_config()
+    @wraps(cmd)
+    def wrapper(*args: t.Any, **kwargs: t.Any) -> None:
+        logger.info("Connecting to database")
+        with psycopg.connect(config.database.connection_string) as connection:
+            logger.info("Connected to database")
+            ctx = Context(config=config, connection=connection)
+            cmd(ctx, *args, **kwargs)
+
+    return wrapper
 
 
 @click.group()
@@ -25,8 +47,8 @@ def cli() -> None:
 
 @cli.command()
 @click.option('-u', '--url', required=True, type=str, help='URL to scrape')
-def scrape(url: str) -> None:
-    config = _load_config()
+@inject_context
+def scrape(ctx: Context, url: str) -> None:
     logging.info("Running scrape command")
     releases = _scrape(url)
     for release in releases:
@@ -34,4 +56,5 @@ def scrape(url: str) -> None:
 
 
 if __name__ == '__main__':
+    logger.info("Starting up")
     cli()
--- aotc/config.py
+++ aotc/config.py
@@ -9,9 +9,24 @@ class SpotifyConfig:
 
 
 @dataclass
+class DatabaseConfig:
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+    @property
+    def connection_string(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}/{self.database}"
+
+
+@dataclass
 class Config:
     title: str
     spotify: SpotifyConfig
+    database: DatabaseConfig
 
     def __post_init__(self) -> None:
         self.spotify = SpotifyConfig(**self.spotify)  # type: ignore
+        self.database = DatabaseConfig(**self.database)  # type: ignore
